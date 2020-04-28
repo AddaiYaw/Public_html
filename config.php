8bb9e2992d40de960c7085447f1bdf35d490f1e8
AddaiYaw <53155875+AddaiYaw@users.noreply.github.com>
2020-04-28 17:25:41 +0200

    initial Git commit Public
    Public repo commitn attempt 1

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "tapoypik_Marc";
+// MySQL Database Password
+$sql_db_pass = "Figaloo123!";
+// MySQL Database Name
+$sql_db_name = "tapoypik_TheMillennialGenDB1";
+
+// Site URL
+$site_url = "http://192.236.209.107"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "3608c5e8-b211-4580-95c2-ba9f943188a5"; // Your purchase code, don't give it to anyone. 
+?>
